db2c1440f893c6e7bbf9216caf1e5e5f7044dab1
Gordan Sajević <gsajevic@hotmail.com>
2014-12-19 17:17:25 +0100

    Create DriverClass.java

---
+++ DriverClass.java
@@ -0,0 +1,23 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+public class DriverClass {
+
+	public static void main(String[] args)
+	{
+		String s = (String) JOptionPane.showInputDialog(new JFrame(),
+				"Enter game full size: ",
+				"Tic Tac Toe size, " + JOptionPane.PLAIN_MESSAGE);
+		try
+		{
+			int size = Integer.parseInt(s);
+			System.out.println(size);
+			TicTacToeGUI g = new TicTacToeGUI(size);
+		} 
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+}
